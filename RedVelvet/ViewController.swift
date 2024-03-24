fb6733e37d9446fa00b2ad7871bdabb8ddafb361
Toby Wang <tobybae91@gmail.com>
2024-03-24 17:47:52 +0800

    Initial Commit

---
+++ RedVelvet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedVelvet
+//
+//  Created by Toby Wang on 2024/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
